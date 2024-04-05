78c7410d23f3f87b5e370cc01789b9c3167c9e3f
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-04-05 14:46:49 +0200

    Add class gift with override compareTo

---
+++ src/org/experis/wishlist/Gift.java
@@ -0,0 +1,29 @@
+package org.experis.wishlist;
+
+public class Gift implements Comparable<Gift> {
+
+    private String name;
+
+    public Gift(java.lang.String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Gift{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+
+    // override metodo compare per controllare il name e ordinarlo con il sort
+    @Override
+    public int compareTo(Gift o) {
+        return this.getName().compareTo(o.getName());
+    }
+}
